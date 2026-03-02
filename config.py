5b3c46aad559654f6886b75d5860f85c357f29a9
CyberWarrior1231 <thestaralwayad@gmail.com>
2026-03-02 17:28:01 +0530

    Update config.py

--- config.py
+++ config.py
@@ -25,10 +25,10 @@ if OWNER_ID not in ADMINS:
 # Your Mongodb Database Url
 # Warning - Give Db uri in deploy server environment variable, don't give in repo.
 DB_URI = os.environ.get("DB_URI", "mongodb+srv://fibegi:8oV4fjNNVasSfcoY@cluster0.jp8thup.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0") # Warning - Give Db uri in deploy server environment variable, don't give in repo.
-DB_NAME = os.environ.get("DB_NAME", "idfinderpro")
+DB_NAME = os.environ.get("DB_NAME", "xyz")
 
 # Force Subscription Channel
-FORCE_SUB_CHANNEL = "idfinderpro"  # Channel username without @
+FORCE_SUB_CHANNEL = "thekmx"  # Channel username without @
 FORCE_SUB_CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002441460670"))
 
 # Log Channel - Set to 0 to disable logging
@@ -42,3 +42,4 @@ ERROR_MESSAGE = bool(os.environ.get('ERROR_MESSAGE', True))
 CRYPTO_PAY_API_TOKEN = os.environ.get("CRYPTO_PAY_API_TOKEN", "")  # Your Crypto Pay API token
 
 CRYPTO_PAY_TESTNET = os.environ.get("CRYPTO_PAY_TESTNET", "False").lower() == "true"  # Set to True for testing with @CryptoTestnetBot
+
